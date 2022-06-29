1d4c1a1f1fc586e537b17d25a4dea0065dbf7d06
KrunalDS <107586754+KrunalDS@users.noreply.github.com>
2022-06-29 12:13:56 -0400

    Update Project1.java

--- Java/project1/src/main/java/loyalist/project1/Project1.java
+++ Java/project1/src/main/java/loyalist/project1/Project1.java
@@ -5,6 +5,6 @@ package loyalist.project1;
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("This is our version 1");
+        System.out.println("This is our version 2 from Krunal");
     }
 }
