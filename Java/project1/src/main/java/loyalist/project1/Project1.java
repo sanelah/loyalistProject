276808146038fd58e54bac5a260669766dbc0c64
Sana <sanelahkhan@loyalistcollege.com>
2022-06-29 12:24:38 -0400

    initial commit

--- Java/project1/src/main/java/loyalist/project1/Project1.java
+++ Java/project1/src/main/java/loyalist/project1/Project1.java
@@ -5,6 +5,14 @@ package loyalist.project1;
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("This is our version 1");
+        System.out.println("This is our version 1");   
+        System.out.println("We will be versioning our java code");   
+        System.out.println("Group Participants");
+        System.out.println("Sunpreet Kaur-----------500188920");
+        System.out.println("Chris Thomas------------500193631");
+        System.out.println("Sanelah Parveen Khan----500192151");
+        System.out.println("Kuldeep Kaur gill-------500192054");
+        System.out.println("Krunal vilas Teli-------500195601");
+        
     }
 }
